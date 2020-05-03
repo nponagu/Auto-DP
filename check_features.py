bd1aa1b74ceb68ad2c2547852a062a4f31b90261
Alexander Tikhonov <nponagu@ya.ru>
2020-05-03 03:22:58 +0300

    add check_spread_var

--- check_features.py
+++ check_features.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def check_category_var(df, valid_values):
+
     """
     Check conditions for a category feature.
     
@@ -32,6 +33,7 @@ def check_category_var(df, valid_values):
 
 
 def check_interval_var(df, min_val=None, max_val=None):
+    
     """
     Check conditions for an interval feature.
     
@@ -67,8 +69,96 @@ def check_interval_var(df, min_val=None, max_val=None):
     return df
 
 
+def check_spread_var(
+    df, 
+    filter_var = None,
+    var_name = "spread",
+    valid_values = list(),
+    na_list = None
+):
+    
+    """
+    Check conditions for an interval feature.
+    
+    Input:
+    1) :df: - pd.DataFrame with a spread feature. Can be numeric or string. Should be with set index
+    2) :filter_var: - bool series for selecting a sample which should to answer
+    3) :var_name: - a short unique name for a list of spread variables
+    4) :valid_values: - expected values for a feature
+    5) :na_list: - values for excluding another values ("don't know", "none of the above" etc.)
+    
+    Output:
+    1) :df: - pd.DataFrame with error variables
+    """
+
+    
+    #create list of spread variables
+    if filter_var:
+        var_list = list(df.columns.drop(filter_var))
+    else:
+        var_list = list(df.columns)
+
+        
+    #create error variables
+    var_name_missing = f"err_{var_name}_missing"
+    var_name_answered = f"err_{var_name}_answered"
+    var_name_values = f"err_{var_name}_values"
+    var_name_na = f"err_{var_name}_NA"
+    var_name_duplicates = f"err_{var_name}_duplicates"
+
+    
+    #missing value
+    df[var_name_missing] = df.loc[
+        df.loc[:, filter_var] == True, var_list].isna().all(axis='columns')
+
+    
+    #a fake answer
+    df[var_name_answered] = ~df.loc[
+        df.loc[:, filter_var] != True, var_list].isna().all(axis='columns')
+
+    
+    #NA with another answer
+    battle_val_list = [val for val in valid_values if val not in na_list]
+    if na_list:
+        df[var_name_na] = (
+            df.loc[:, var_list].isin(na_list).any(axis='columns') & 
+            df.loc[:, var_list].isin(battle_val_list).any(axis='columns')
+        )
+    
+    
+    #duplicates
+    df[var_name_duplicates] = False
+    for num in range(len(var_list) - 1):
+        var = var_list[num]
+        short_mult_list = var_list[num+1:]
+        for val in valid_values:
+            bool_array = (
+                df.loc[:, [var]].isin([val]).any(axis='columns') & 
+                df.loc[:, short_mult_list].isin([val]).any(axis='columns')
+                )
+            df[var_name_duplicates].loc[bool_array] = True
+
+            
+    return df
+
+
 if __name__ == "__main__":
     
-    df = pd.read_excel("Межнац финал.xlsx", usecols=["NUMBER", "GORSELO"], index_col="NUMBER")
-    checked_df = check_category_var(df, [1,2])
-    print(checked_df.columns)
+    # df = pd.read_excel("Межнац финал.xlsx", usecols=["NUMBER", "GORSELO"], index_col="NUMBER")
+    # checked_df = check_category_var(df, [1,2])
+
+    df = pd.read_excel("Межнац финал.xlsx", usecols=[
+        "NUMBER", "V24", "V25.1", "V25.2", "V25.3", "V25.4", "V25.5", "V25.6", "V25.7", "V25.8"
+        ], index_col="NUMBER")
+    df["filt_V25"] = (df.loc[:, "V24"] == 1)
+    df.drop(columns=["V24"], inplace=True)
+
+
+    checked_df = check_spread_var(
+        df,
+        filter_var="filt_V25",
+        var_name="V25",
+        na_list=[99], 
+        valid_values=[1,2,3,4,5,6,7,8,9,99]
+        )
+    print(checked_df.shape)
